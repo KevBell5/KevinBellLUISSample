c6d680b1fc56b355612eb466882f7ad74e3c2a36
Kevin Bell <accessui@outlook.com>
2018-09-22 08:00:55 -0700

    Add LUIS to Bot

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Bot.Builder.Dialogs;
@@ -12,11 +14,33 @@ namespace Microsoft.Bot.Sample.LuisBot
     [Serializable]
     public class BasicLuisDialog : LuisDialog<object>
     {
+        // CONSTANTS        
+        // Entity
+        public const string Entity_Device = "HomeAutomation.Device";
+        public const string Entity_Room = "HomeAutomation.Room";
+        public const string Entity_Operation = "HomeAutomation.Operation";
+
+        // Intents
+        public const string Intent_TurnOn = "HomeAutomation.TurnOn";
+        public const string Intent_TurnOff = "HomeAutomation.TurnOff";
+        public const string Intent_None = "None";
+
         public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
             ConfigurationManager.AppSettings["LuisAppId"], 
             ConfigurationManager.AppSettings["LuisAPIKey"], 
             domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
         {
+        }
+        [LuisIntent(Intent_TurnOn)]
+        public async Task OnIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent(Intent_TurnOff)]
+        public async Task OffIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
         }
 
         [LuisIntent("None")]
@@ -25,30 +49,37 @@ namespace Microsoft.Bot.Sample.LuisBot
             await this.ShowLuisResult(context, result);
         }
 
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "Greeting" with the name of your newly created intent in the following handler
-        [LuisIntent("Greeting")]
-        public async Task GreetingIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
-        [LuisIntent("Cancel")]
-        public async Task CancelIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
-        [LuisIntent("Help")]
-        public async Task HelpIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
         {
-            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            // get recognized entities
+            string entities = this.BotEntityRecognition(result);
+
+            // round number
+            string roundedScore = result.Intents[0].Score != null ? (Math.Round(result.Intents[0].Score.Value, 2).ToString()) : "0";
+
+            await context.PostAsync($"**Query**: {result.Query}, **Intent**: {result.Intents[0].Intent}, **Score**: {roundedScore}. **Entities**: {entities}");
             context.Wait(MessageReceived);
+        }
+
+        // Entities found in result
+        public string BotEntityRecognition(LuisResult result)
+        {
+            StringBuilder entityResults = new StringBuilder();
+
+            if (result.Entities.Count > 0)
+            {
+                foreach (EntityRecommendation item in result.Entities)
+                {
+                    // Query: Turn on the [light]
+                    // item.Type = "HomeAutomation.Device"
+                    // item.Entity = "light"
+                    entityResults.Append(item.Type + "=" + item.Entity + ",");
+                }
+                // remove last comma
+                entityResults.Remove(entityResults.Length - 1, 1);
+            }
+
+            return entityResults.ToString();
         }
     }
 }
