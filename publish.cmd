2ba08160eb7fc0672ebd10c243dd76f576c75d97
Kevin Bell <accessui@outlook.com>
2018-09-22 07:37:41 -0700

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=kevbellluissample-Web-Deploy.pubxml -p:Password=nhMWbqHuGz3E8QPTkpytcpQbfbLdawrv0vcvEvHDh3CMAQd1n11padi73mMW
+
